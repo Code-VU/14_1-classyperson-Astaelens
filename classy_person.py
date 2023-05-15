1616fef1549b8f29d450bc7f0b0fd2b8e8b5d8df
Alex Staelens <Alex.Staelens@vu.com>
2023-05-15 17:00:07 -0500

    Created methods for Person class

--- classy_person.py
+++ classy_person.py
@@ -8,8 +8,17 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
-
-
-
+    
+    def __init__(self,age,name):
+        self.age=age  
+        self.name=name
+    def increase_age(self):
+        self.age=self.age+1
+        
+    def say_greeting(self):
+        print(f'Hello world! My name is {self.name}!')
+    def count_to_age(self):
+        for i in range(self.age):
+            print(i+1)
 
 # You won't need to call anything here.
